4b236afa3850f2dfd334f5a0f00ec53a42cfaf62
cszczepaniak <cszczepaniak@gmail.com>
2023-06-18 13:09:30 -0500

    get mysql setup locally + migrations

---
+++ prisma/migrations/20230618174714_initial_migration/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE `Player` (
+    `id` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `teamID` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Team` (
+    `id` VARCHAR(191) NOT NULL,
+    `divisionID` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Division` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Game` (
+    `id` VARCHAR(191) NOT NULL,
+    `team1ID` VARCHAR(191) NOT NULL,
+    `team2ID` VARCHAR(191) NOT NULL,
+    `kind` INTEGER NOT NULL,
+    `winner` VARCHAR(191) NULL,
+    `loserScore` INTEGER NULL,
+    `divisionID` VARCHAR(191) NOT NULL,
+
+    INDEX `Game_team2ID_idx`(`team2ID`),
+    INDEX `Game_winner_idx`(`winner`),
+    INDEX `Game_kind_idx`(`kind`),
+    UNIQUE INDEX `Game_team1ID_team2ID_kind_key`(`team1ID`, `team2ID`, `kind`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Admin` (
+    `email` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
