3389a2f98637f02522e321395f9eb09d8d178c69
cszczepaniak <cszczepaniak@gmail.com>
2023-06-28 21:03:58 -0500

    be able to generate and get games

--- src/routes/admin/games/+page.server.ts
+++ src/routes/admin/games/+page.server.ts
@@ -1,5 +1,97 @@
+import { fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { prisma } from '$lib/server/db/client';
+import { generatePrelimGames } from '$lib/server/games/games';
+import { gameKindPrelim } from '$lib/server/utils/games';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+const getPrelimGames = async () => {
+    const games = await prisma.game.findMany({
+        select: {
+            id: true,
+            division: true,
+            team1ID: true,
+            team2ID: true,
+            loserScore: true,
+            winner: true,
+        },
+        where: {
+            kind: gameKindPrelim,
+        },
+    });
+
+    const teams = (
+        await prisma.team.findMany({
+            select: {
+                id: true,
+                players: {
+                    select: {
+                        firstName: true,
+                        lastName: true,
+                    },
+                },
+            },
+        })
+    ).reduce(
+        (prev, t) => {
+            prev.set(t.id, t);
+            return prev;
+        },
+        new Map<
+            string,
+            {
+                id: string;
+                players: { firstName: string; lastName: string }[];
+            }
+        >()
+    );
+
+    return games.map((g) => ({
+        ...g,
+        teams: [teams.get(g.team1ID), teams.get(g.team2ID)],
+    }));
+}
+
+export const load: PageServerLoad = async () => {
+    let games = await getPrelimGames();
+    return { games };
+};
+
+export const actions: Actions = {
+    createPrelims: async () => {
+        const divs = await prisma.division.findMany({
+            select: {
+                id: true,
+                name: true,
+                teams: {
+                    select: {
+                        id: true,
+                    },
+                },
+            },
+        });
+
+        let invalid = divs
+            .filter(d => d.teams.length !== 4 && d.teams.length !== 6);
+
+        if (invalid.length > 0) {
+            return fail(400, {
+                "create": invalid.map(d => ({ name: d.name, nTeams: d.teams.length })),
+            });
+        }
+
+        const games = generatePrelimGames(divs);
+        console.log(games.size);
+
+        await prisma.game.createMany({
+            data: Array.from(games.entries()).flatMap(([divID, pairs]) =>
+                pairs.map((pair) => ({
+                    kind: gameKindPrelim,
+                    team1ID: pair[0],
+                    team2ID: pair[1],
+                    divisionID: divID,
+                }))
+            ),
+            skipDuplicates: true,
+        });
+    },
+}
--- src/routes/admin/games/+page.svelte
+++ src/routes/admin/games/+page.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
+	import { page } from "$app/stores";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	let invalidDivisions: { name: string; nTeams: number }[] = [];
+	$: {
+		invalidDivisions = $page.form?.create;
+	}
 </script>
 
 <h1>Games</h1>
+<form method="POST" action="?/createPrelims" use:enhance>
+	<button>Create Prelims</button>
+</form>
+
+{#if invalidDivisions?.length > 0}
+	<p>The following divisions were invalid:</p>
+	<ul>
+		{#each invalidDivisions as division}
+			<li>{`${division.name} (${division.nTeams} teams)`}</li>
+		{/each}
+	</ul>
+{/if}
+
+{#each data.games as game}
+	<div>
+		{game.teams
+			.map((team) => team?.players.map((p) => `${p.firstName} ${p.lastName}`).join(", "))
+			.join(" VS ")}
+	</div>
+{/each}
