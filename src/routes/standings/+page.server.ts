d28b27dd47fab1396435124b20009c7dc22c10b3
cszczepaniak <cszczepaniak@gmail.com>
2023-07-02 15:41:09 -0500

    build out standings

--- src/routes/standings/+page.server.ts
+++ src/routes/standings/+page.server.ts
@@ -1,15 +1,38 @@
 import { prisma } from "$lib/server/db/client";
 import type { Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { gameKindPrelim } from "$lib/server/utils/games";
 
 export const load: PageServerLoad = async () => {
     const teams = await prisma.team.findMany({
         select: {
+            id: true,
             division: true,
             players: true,
         }
     });
-    return { teams };
+
+    const games = await prisma.game.findMany({
+        where: {
+            kind: gameKindPrelim,
+        },
+    })
+
+    const completedGames = games.filter(g => g.loserScore !== null && g.winner !== null);
+
+    return {
+        teams: teams.map(t => ({
+            ...t,
+            wins: completedGames.filter(g => g.winner === t.id).length,
+            losses: completedGames.filter(g => (g.team1ID === t.id || g.team2ID === t.id) && g.winner !== t.id).length,
+            totalScore: completedGames.filter(g => g.team1ID === t.id || g.team2ID === t.id).reduce((score, g) => {
+                if (g.winner === t.id) {
+                    return score + 121
+                }
+                return score + (g.loserScore ?? 0)
+            }, 0)
+        })),
+    };
 };
 
 export const actions: Actions = {}
--- src/routes/standings/+page.svelte
+++ src/routes/standings/+page.svelte
@@ -11,6 +11,44 @@
 	import { getTeamName } from "$lib/utils/teams";
 
 	export let data: PageData;
+
+	const hasNoGames = (t: (typeof data.teams)[number]) => t.losses + t.wins + t.totalScore === 0;
+
+	let teams: typeof data.teams = [];
+	$: {
+		teams = data.teams.sort((a, b) => {
+			// Put teams with no recorded games at the bottom.
+			if (hasNoGames(a) && !hasNoGames(b)) {
+				return 1;
+			} else if (!hasNoGames(a) && hasNoGames(b)) {
+				return -1;
+			}
+
+			// Put teams with the highest number of wins furthest at the top.
+			if (a.wins > b.wins) {
+				// More wins towards the top.
+				return -1;
+			} else if (a.wins < b.wins) {
+				return 1;
+			}
+
+			// Put teams with fewer losses next.
+			if (a.losses < b.losses) {
+				return -1;
+			} else if (a.losses > b.losses) {
+				return 1;
+			}
+
+			// Then sort by total score.
+			if (a.totalScore > b.totalScore) {
+				return -1;
+			} else if (a.totalScore < b.totalScore) {
+				return 1;
+			}
+
+			return 0;
+		});
+	}
 </script>
 
 <h1 class="text-3xl mb-4">Prelim Standings</h1>
@@ -24,13 +62,13 @@
 		<TableHeadCell>Total Score</TableHeadCell>
 	</TableHead>
 	<TableBody>
-		{#each data.teams as team}
+		{#each teams as team}
 			<TableBodyRow>
 				<TableBodyCell>{getTeamName(team.players)}</TableBodyCell>
 				<TableBodyCell>{team.division?.name ?? "No Division"}</TableBodyCell>
-				<TableBodyCell>0</TableBodyCell>
-				<TableBodyCell>0</TableBodyCell>
-				<TableBodyCell>0</TableBodyCell>
+				<TableBodyCell>{team.wins}</TableBodyCell>
+				<TableBodyCell>{team.losses}</TableBodyCell>
+				<TableBodyCell>{team.totalScore}</TableBodyCell>
 			</TableBodyRow>
 		{/each}
 	</TableBody>
