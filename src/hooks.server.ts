20d0f57ed308cfd6df63d072b27417503bce0fa1
cszczepaniak <cszczepaniak@gmail.com>
2023-07-05 18:36:36 -0500

    :wave: trpc

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,10 @@
-import { createContext } from "$lib/server/trpc/context";
-import { router } from "$lib/server/trpc/router";
-import type { Handle } from "@sveltejs/kit";
-import { createTRPCHandle } from "trpc-sveltekit";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/core/providers/google";
 import type { Provider } from "@auth/core/providers";
 import type { Profile } from "@auth/core/types";
-import { sequence } from "@sveltejs/kit/hooks";
 import { env } from "$env/dynamic/private";
 
-const trpcHandle: Handle = createTRPCHandle({ router, createContext });
-const authHandle = SvelteKitAuth({
+export const handle = SvelteKitAuth({
 	providers: [
 		Google({
 			clientId: env.GOOGLE_ID,
@@ -18,5 +12,3 @@ const authHandle = SvelteKitAuth({
 		}) as Provider<Profile>,
 	],
 });
-
-export const handle = sequence(authHandle, trpcHandle);
