5777aa8339ece0aceec0400c68b7cb176beb084c
cszczepaniak <cszczepaniak@gmail.com>
2023-07-11 20:59:43 -0500

    save to local storage

--- src/lib/stores/devtools.ts
+++ src/lib/stores/devtools.ts
@@ -1,3 +1,13 @@
+import { browser } from "$app/environment";
 import { writable } from "svelte/store";
 
-export const devTools = writable(false);
+export const devTools = writable(
+	browser && localStorage.getItem("cribbly-svelte.devtools") === "true" ? true : false,
+);
+
+devTools.subscribe(val => {
+	if (!browser) {
+		return;
+	}
+	localStorage.setItem("cribbly-svelte.devtools", val ? "true" : "false");
+});
